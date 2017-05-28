539ec831530ba366ca5fd3650395b19b0ec12ecb
gp22 <gp22@tutanota.com>
2017-05-28 11:14:13 +0700

    add socket.io events

--- server/server.js
+++ server/server.js
@@ -12,6 +12,19 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+// Setup a listener for a connection.
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('client disconnected');
+  });
+});
+
+/*
+Call server.listen instead of app.listen to use the http server instead
+of the express server. This is necessary in order to use socket.io.
+*/
 server.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
 });
